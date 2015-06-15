f23f37fd29ff35638d905c68d87877eb6fc609a0
Dragouf <da.laurent@laposte.net>
2015-06-15 23:05:33 +0900

    - functionnal version

---
+++ QRCodeUI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var t = new ZXing.QrCode.QRCodeWriter();
+            var options = new Dictionary<ZXing.EncodeHintType, object> { 
+                { ZXing.EncodeHintType.CHARACTER_SET, "utf8" },
+                { ZXing.EncodeHintType.ERROR_CORRECTION, "" },
+                { ZXing.EncodeHintType.DISABLE_ECI, false }
+            };
+            var matrix = t.encode(textBox1.Text, 100, 100, options);
+
+            this.pictureBox1.Image = t.ToBitmap(matrix);
+
+            this.textBox2.Text = matrix.ToString();
+        }
+    }
+}
